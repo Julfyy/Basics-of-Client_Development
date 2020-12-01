e8370d36c1c6843ccfffd19aa1b3462670258a25
Bohdan <bohdan7565@gmail.com>
2020-12-01 13:21:43 +0200

    4th lab done

--- lab4/script/script.js
+++ lab4/script/script.js
@@ -1,132 +1,128 @@
-// // var block2 = document.getElementById('col-2');
-// var block3 = document.getElementById('col-3');
-// var block4 = document.getElementById('col-4');
-// var block5 = document.getElementById('col-5');
-// // var block6 = document.getElementById('col-6');
+var block1 = document.querySelector('header');
+var block2 = document.querySelector('#col-2');
+var block3 = document.querySelector('#col-3');
+var block4 = document.querySelector('#col-4');
+var block5 = document.querySelector('#col-5');
+var block6 = document.querySelector('#col-6');
+var block7 = document.querySelector('footer');
+var form = document.querySelector('form');
+let div = document.querySelector("#git-output");
+let inputArray = document.querySelector("#numbers-input");
+
+var blocks = [block1, block2, block3, block4, block5, block6, block7];
 
 
-// (function task1() {
-//     let temp = block5.innerHTML;
-//     block5.innerHTML = block4.innerHTML;
-//     block4.innerHTML = temp;
-// })();
+function task1() {
+    let delay = 500;
 
-// var a = 3,
-//     b = 4,
-//     c = 5;
+    function swapContent(index) {
+        [blocks[index].innerHTML, blocks[(index - 1) % 6].innerHTML] = [blocks[(index - 1) % 6].innerHTML, blocks[index].innerHTML];
 
-// (function task2(a, b, c) {
-//     let p = (a + b + c) / 2;
-//     let s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-//     block3.firstElementChild.innerHTML += `<h2> Triangle area is: ${s.toFixed(2)} </h2>`;
-// })(a, b, c);
+        if (index < 6) {
+            delay += 500;
+            setTimeout(swapContent, delay, ++index);
+        }
+    };
 
-// (function task3() {
-//     window.onload = function() {
-//         let cookies = document.cookie.split(';');
-//         if (cookies.length > 0) {
-//             alert('Cookies will be deleted after pressing "Ok" button');
-//             cookies.map(item => {
-//                 document.cookie = item.substring(item.indexOf('=')) + '=;';
-//             });
-//             alert("Cookies were deleted!");
-//             // location.reload();
-//         }
-//     }
-//     document.querySelector('#submit').onclick = function() {
-//         let input = document.querySelector('#number-input').value;
-//         let numbers = input.trim().split(' ').map(x => parseInt(x, 10));
-//         let maxElem = Math.max.apply(Math, numbers);
-//         let counter = 0;
-//         numbers.forEach(elem => {
-//             if (maxElem == elem) {
-//                 counter++;
-//             }
-//         });
-//         document.cookie = `max=${maxElem}`;
-//         document.cookie = `times = ${counter}`;
-//         if (maxElem != NaN) {
-//             alert(`Element ${maxElem} is max, occured ${counter} times`);
-//         }
-
-//     };
-// })();
-
-// (function task4() {
-//     let fontWeightValue = localStorage.getItem('font-weight');
-//     if (fontWeightValue) {
-//         document.querySelector('#col-6').style.fontWeight = fontWeightValue;
-//         document.querySelector(`#${fontWeightValue}`).checked = 'true';
-//     }
-
-//     document.querySelector('#bold').onclick = function() {
-//         document.querySelector('#col-6').style.fontWeight = 'bold';
-//         localStorage.setItem('font-weight', 'bold');
+    setTimeout(swapContent, delay, 1);
+}
 
 
-//     };
-//     document.querySelector('#normal').onclick = function() {
-//         document.querySelector('#col-6').style.fontWeight = 'normal';
-//         localStorage.setItem('font-weight', 'normal');
-//     };
-// })();
 
-// (function task5() {
-//     window.addEventListener('scroll', function(e) {
-//         if (document.body.scrollTop > 0) {
-//             document.querySelector('#col-5').style.backgroundColor = 'greenyellow';
-//         } else {
-//             document.querySelector('#col-5').style.backgroundColor = 'orange';
 
-//         }
-//     });
-// })();
+(function task2() {
 
-// function addInputedText(block) {
-//     let content = localStorage.getItem(block.id);
+    window.addEventListener('scroll', function() {
+        setTimeout(function() {
+            if (block1.style.fontStyle == 'italic') {
+                block1.style.fontStyle = '';
+                block6.style.fontStyle = '';
+            } else {
+                block1.style.fontStyle = 'italic';
+                block6.style.fontStyle = 'italic';
+            }
+        }, 5000);
 
-//     if (content) {
-//         let backup = block.innerHTML;
-//         block.innerHTML = content;
 
-//         let button = document.createElement('button');
-//         button.textContent = 'submit';
-//         button.id = 'btnSubmit';
-//         button.onclick = () => {
-//             block.innerHTML = backup;
-//             localStorage.removeItem(block.id);
-//             loadRegularText(block);
-//         };
+    });
+    setInterval(() => {
+        return block5.style.fontStyle == 'italic' ?
+            block5.style.fontStyle = '' : block5.style.fontStyle = "italic"
 
-//         block.appendChild(button);
-//     }
-// };
+    }, 5000)
 
-// function loadRegularText(block) {
-//     let inputArea = document.createElement('input');
-//     inputArea.textContent = block.innerHTML;
-//     inputArea.style.width = '100px';
-//     inputArea.oninput = () => {
-//         localStorage.setItem(block.id, inputArea.value);
-//     };
 
-//     block.appendChild(inputArea);
-// };
-// (function task6() {
-//     let blocks = [document.querySelector('header'),
-//         document.querySelector('#col-2'),
-//         document.querySelector('#col-3'),
-//         document.querySelector('#col-4'),
-//         document.querySelector('#col-5'),
-//         document.querySelector('#col-6')
-//     ];
+})();
 
-//     blocks.forEach(block => {
-//         if (localStorage.getItem(block.id)) {
-//             addInputedText(block);
-//         } else {
-//             loadRegularText(block);
-//         }
-//     });
 
-// })();
+//Не працює після того як видає помилку, треба оновлювать
+
+function task3() {
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let name = document.querySelector('#name-input').value;
+        let repos_name = document.querySelector('#repository-name').value;
+        let url = `https://api.github.com/repos/${name}/${repos_name}/commits`;
+        fetch(url).then((res) => {
+            if (!res.ok) {
+                div.innerHTML = 'Error:  ' + res.statusText;
+                div.style.backgroundColor = 'red';
+                div.style.height = '100px';
+                let button = document.createElement("button");
+                button.textContent = "OK";
+                button.addEventListener("click", () => { div.style.display = "none"; });
+                div.appendChild(button);
+            }
+            return res.json();
+
+        }).then((content) => {
+            content.forEach((item) => {
+                let li = document.createElement('li');
+                let str = item.commit.author.name + ": " + item.commit.message;
+                li.innerHTML = str;
+                div.appendChild(li);
+            });
+        });
+    });
+}
+
+task3();
+
+let callback1 = function(callback) {
+    console.log("Callback 1");
+    callback();
+};
+
+let callback2 = function() {
+    console.log("Callback 2");
+};
+
+function task4(cb1, cb2) {
+    cb1(cb2);
+};
+task4(callback1, callback2);
+
+
+function bubbleSort(inputArr) {
+    let len = inputArr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (inputArr[j] > inputArr[j + 1]) {
+                let tmp = inputArr[j];
+                inputArr[j] = inputArr[j + 1];
+                inputArr[j + 1] = tmp;
+            }
+        }
+    }
+    return inputArr;
+};
+
+let task5 = () => {
+    document.querySelector('#sort-button').onclick = () => {
+        let arr = inputArray.value.match(/-?\d+\.?\d*/g).map(Number);
+        alert(bubbleSort(arr));
+        inputArray.value = '';
+    };
+};
+task5();
